14ef4a9c2f9a3ec73c12f7857124cc0d58489e3a
KANG <wind_breath@naver.com>
2022-10-25 13:09:11 +0900

    edit : BidService

--- src/main/java/com/mini/auction/dto/response/BidResponseDto.java
+++ src/main/java/com/mini/auction/dto/response/BidResponseDto.java
@@ -1,20 +1,11 @@
 package com.mini.auction.dto.response;
 
 import com.mini.auction.domain.Bid;
-import com.mini.auction.domain.Member;
-import com.mini.auction.domain.Product;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class BidResponseDto {
 
     private Integer biddingPrice;
@@ -25,21 +16,15 @@ public class BidResponseDto {
 
     private String username;
 
-    List<String> bidParticipants;
+    private List<String> usernameList;
 
-    public static List<String> getParticipantList(List<Product> productList) {
-        List<String> participantList = new ArrayList<>();
-        for (Product product : productList) {
-            participantList.add(product.getMember().getUsername());
-        }
-        return participantList;
-    }
 
-    public BidResponseDto(Bid bid, Product product, Member member) {
-        this.biddingPrice = bid.getBiddingPrice();
-        this.title = product.getTitle();
-        this.username = member.getUsername();
-        this.participantCnt = bidParticipants.size();
+    public  BidResponseDto(Bid bid, List<String> usernameList) {
+        this.biddingPrice = bid.getProduct().getHighPrice();
+        this.title = bid.getProduct().getTitle();
+        this.username = bid.getMember().getUsername();
+        this.usernameList = usernameList;
+        this.participantCnt = usernameList.size();
     }
 
 }
